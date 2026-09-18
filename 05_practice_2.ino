37ca14a5a6e7feb4f067b7dd4b50ce6d0bff5f69
B0707 <uiqnhuiys@gmail.com>
2026-09-18 10:41:51 +0900

    소프트웨어프로젝트2 과제 05P14

---
+++ 05_practice_2.ino
@@ -0,0 +1,21 @@
+const int LED_PIN = 7;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_PIN, HIGH);   // 꺼짐
+    delay(100);
+    digitalWrite(LED_PIN, LOW);  // 켜짐
+    delay(100);
+  }
+
+  digitalWrite(LED_PIN, HIGH);
+  while (1) {
+  }
+}
